2c30314bd17c664ed4ba1e08df95f2f44891c9c5
codelint <codelint@foxmail.com>
2016-11-03 18:09:56 +0800

    推广栏目，显示用户关注的旅行家的最新路点信息

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,31 @@
 
 jQuery(function($){
 
+    function loadBarData(callback){
+//        callback(false, [
+//            {
+//                'src': '../images/img.jpg?oss_image_style=1024w_480h_4e_250-248-236bgc',
+//                'href': '../images/img.jpg?oss_image_style=1024w_480h_4e_250-248-236bgc'
+//            }
+//        ]);
+        var user = android.get('user.current');
+        var traveller_id = (user && user['source']) ? user['source'] : 0;
+        U.api.checkpoint.list({'group_id': 0, 'traveller_id': traveller_id}, function(err, json){
+            var arr = [];
+            var checkpoint;
+            if(json){
+                for(var i = json.length; i--;){
+                    checkpoint = json[i];
+                    arr.push({
+                        'src': U.api.oss.rid2url(checkpoint['photo'], '1024w_480h_4e_250-248-236bgc'),
+                        'href': U.api.oss.rid2url(checkpoint['photo'], '1024w_480h_4e_250-248-236bgc')
+                    });
+                }
+                callback(err, arr);
+            }
+        })
+    }
+
     /**
      *
      * @param callback
@@ -21,7 +46,7 @@ jQuery(function($){
 //                portrait: '../images/img.jpg?oss_image_style=1024w'
 //            }
 //        ]);
-        U.api.checkpoint.list({'group_id' : 0}, function(err, json){
+        U.api.checkpoint.list({'group_id': 0}, function(err, json){
             var arr = [];
             var checkpoint;
             if(json){
@@ -32,7 +57,7 @@ jQuery(function($){
                         'dateTime': checkpoint['created_at'],
                         'day': 4,
                         'browse': 0,
-                        'by': checkpoint['user_id'],
+                        'by': checkpoint['user']['nick'],
                         'img': U.api.oss.rid2url(checkpoint['photo'], '1024w_768w'),
                         'portrait': U.api.oss.rid2url(checkpoint['photo'], '1024w_768w'),
                         'place': checkpoint['longitude'] + ',' + checkpoint['latitude']
@@ -56,15 +81,34 @@ jQuery(function($){
         })
     });
 
+    loadBarData(function(err, data){
+        if(err){
+            alert(err.message);
+            return;
+        }
+        var len = data.length;
+        alert(len);
+        var item;
+        var $div;
+        for(var i = 0; i < len; i++){
+            item = data[i];
+            $div = $('<div class="swiper-slide">' +
+                '<img src="../images/img.jpg?oss_image_style=1024w_480h_4e_250-248-236bgc" alt="">' +
+                '</div>');
+            $div.find('img').attr('src', item['src']).attr('href', item['href']);
+            $('div.swiper-wrapper').append($div);
+        }
+        $(".barn").swiper({
+            loop: true,
+            autoplay: 2000,//可选选项，自动滑动
+            pagination: '.swiper-pagination',// 分页
+            autoHeight: true, //高度随内容变化
+            autoplayDisableOnInteraction: false// 点击后继续滑动
+        });
+    });
+
     var $leftMenu = $(".leftMenu");
 
-    $(".barn").swiper({
-        loop: true,
-        autoplay: 2000,//可选选项，自动滑动
-        pagination: '.swiper-pagination',// 分页
-        autoHeight: true, //高度随内容变化
-        autoplayDisableOnInteraction: false// 点击后继续滑动
-    });
 
     $(".on-menu").click(function(){
         $leftMenu.css("left", "0");
