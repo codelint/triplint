a227f510d72dd16a4db6f3295283d6793336a760
codelint <codelint@foxmail.com>
2016-12-04 02:40:59 +0800

    enhance

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp'),
-    // jshint = require('gulp-jshint'),
+// jshint = require('gulp-jshint'),
     cleanCSS = require('gulp-clean-css'),
     less = require('gulp-less'),
     path = require('path'),
+    uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     watch = require('gulp-watch'),
     rename = require('gulp-rename');
 
 
-gulp.task('dist', ['less', 'minify-css', 'copy', 'copy-public'], function () {
+gulp.task('dist', ['less', 'minify-css', 'copy', 'copy-public'], function(){
 });
 // 编译less
-gulp.task('less', function () {
+gulp.task('less', function(){
     return gulp.src(['src/less/*.less', '!src/less/variable.less'])
         .pipe(less({
             paths: [path.join(__dirname, 'less', 'includes')]
@@ -20,19 +21,19 @@ gulp.task('less', function () {
         .pipe(gulp.dest('src/css'));
 });
 
-gulp.task('copy', function () {
+gulp.task('copy', function(){
     var start = './src/**';
     return gulp.src(start)
         .pipe(gulp.dest('dist'));
 });
 // copy public
-gulp.task('copy-public', function () {
+gulp.task('copy-public', function(){
     var start = './public/**';
     return gulp.src(start)
         .pipe(gulp.dest('dist/public'));
 });
 // 压缩css
-gulp.task('minify-css', ['less'], function () {
+gulp.task('minify-css', ['less'], function(){
     return gulp.src(['!src/css/variable.css', 'src/css/*.css'])
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
@@ -40,10 +41,17 @@ gulp.task('minify-css', ['less'], function () {
 });
 
 // 合并
-gulp.task('concat', ['minify-css'], function () {
+gulp.task('concat', ['minify-css'], function(){
     return gulp.src('src/css/*.css')
         .pipe(concat('all.css'))
         .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('uglify-js', function(){
+    return gulp.src(['src/js/*.js'])
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'));
 });
 
 // 检查js
@@ -54,11 +62,11 @@ gulp.task('concat', ['minify-css'], function () {
  });*/
 
 // 监视
-gulp.task('watch', ['dist'], function () {
+gulp.task('watch', ['dist'], function(){
     gulp.watch('src/**/*.*', ['dist']);
 });
 
 // 默认task
-gulp.task('default', ['watch'], function () {
+gulp.task('default', ['watch'], function(){
     // 将你的默认的任务代码放在这
 });
