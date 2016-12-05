b201a7bc9435091a14390438d720268d33cfb08c
codelint <codelint@foxmail.com>
2016-12-06 03:04:03 +0800

    enhance

---
+++ bin/build.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+function log_info(){
+    echo "[$(date '+%F %T')] "$@
+}
+log_info "current system: $(uname | tr [A-Z] [a-z])"
+case $(uname | tr [A-Z] [a-z]) in
+    "darwin")
+        function readlink(){
+            greadlink "$@"
+        }
+        function sed(){
+            gsed "$@"
+        }
+        ;;
+    *)
+        ;;
+esac
+
+SELF=$(readlink -f $0)
+DIR=$(dirname "$SELF")
+
+script_to_js_src "$WORKSPACE/dist/view/trac/ticket.html"
+
+$gulp_cmd prod
+if [ $? -gt 0 ];then
+    return 1
+fi
+
+# combine js
+combile_dist_js "$WORKSPACE/dist/view/upload.html"
+combile_dist_js "$WORKSPACE/dist/view/index.html"
+combile_dist_js "$WORKSPACE/dist/view/trips.html"
+combile_dist_js "$WORKSPACE/dist/view/trac/ticket.html"
+
